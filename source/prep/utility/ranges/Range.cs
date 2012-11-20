dac65d29f0d8f79ff49515359ff351d48da3a629
Ryan Marcotte <ryanm@iqmetrix.com>
2012-11-19 17:29:10 -0800

    Pushing new changes

---
+++ source/prep/utility/ranges/Range.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.utility.ranges
+{
+	public class Range<T> : IContainValues<T> where T : IComparable<T>
+	{
+		public bool IncludesStart { get; set; }
+		public T StartOfRange { get; set; }
+		public bool IncludesEnd { get; set; }
+		public T EndOfRange { get; set; }
+
+		public Range(T min, T max)
+		{
+			this.IncludesStart = true;
+			this.StartOfRange = min;
+			this.IncludesEnd = true;
+			this.EndOfRange = max;
+		}
+
+		public bool contains(T item)
+		{
+			if((this.IncludesStart) && (this.IncludesEnd))
+				return (item.CompareTo(this.StartOfRange) >= 0 && item.CompareTo(this.EndOfRange) <= 0);
+			else if ((this.IncludesStart) && (!this.IncludesEnd))
+				return (item.CompareTo(this.StartOfRange) >= 0 && item.CompareTo(this.EndOfRange) < 0);
+			else if ((!this.IncludesStart) && (this.IncludesEnd))
+				return (item.CompareTo(this.StartOfRange) > 0 && item.CompareTo(this.EndOfRange) <= 0);
+			else
+				return (item.CompareTo(this.StartOfRange) > 0 && item.CompareTo(this.EndOfRange) < 0);
+			
+		}
+	}
+}
---
+++ source/prep/utility/ranges/RangeBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.utility.ranges
+{
+	public class RangeBuilder<T> where T : IComparable<T>
+	{
+		Range<T> _range;
+
+		public RangeBuilder(T min, T max)
+		{
+			_range = new Range<T>(min, max);
+		}
+
+		public RangeBuilder<T> less_than_or_equal_to(T value)
+		{
+			_range.IncludesEnd = true;
+			_range.EndOfRange = value;
+			return (this);
+		}
+
+		public RangeBuilder<T> greater_than_or_equal_to(T value)
+		{
+			_range.IncludesStart = true;
+			_range.StartOfRange = value;
+			return (this);
+		}
+
+		public RangeBuilder<T> less_than(T value)
+		{
+			_range.IncludesEnd = false;
+			_range.EndOfRange = value;
+			return (this);
+		}
+
+		public RangeBuilder<T> greater_than(T value)
+		{
+			_range.IncludesStart = false;
+			_range.StartOfRange = value;
+			return (this);
+		}
+
+		public Range<T> range()
+		{
+			return (_range);
+		}
+	}
+}
